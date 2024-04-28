3ec52a3fa07778a9dea1e5498f3371d9f7fb6362
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2024-04-28 18:17:12 -0400

    Created cli interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,15 @@
-mod server;
+use std::path::PathBuf;
+
+use clap::Parser;
+
+pub mod server;
 pub mod spec;
+
+#[derive(Parser)]
+#[clap(version = "0.1.0", author = "Thiago Pacheco")]
+pub struct Args {
+    #[clap(index = 1)]
+    pub spec: PathBuf,
+    #[clap(short, long, default_value = "8080")]
+    pub port: Option<u16>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,24 @@
-fn main() {}
+use actix_web::{web, App, HttpServer};
+use clap::Parser;
+use openapi_mocker::{
+    server::{get_scope, AppState},
+    Args,
+};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+    println!("Starting server with spec: {}", args.spec.display());
+
+    let port = args.port.unwrap_or(8080);
+    let spec = oas3::from_path(&args.spec).expect("failed to load spec");
+    let data = web::Data::new(AppState { spec });
+
+    let server = HttpServer::new(move || App::new().app_data(data.clone()).service(get_scope()))
+        .bind(("127.0.0.1", port))
+        .expect("Failed to bind to port");
+
+    server.run().await.expect("Failed to run server");
+
+    Ok(())
+}
