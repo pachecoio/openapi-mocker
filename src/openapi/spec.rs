2f535ce84263bd5bcae1ffeb7780349af117a47c
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2024-05-01 19:49:37 -0400

    Included support to query examples by path or query params

--- src/openapi/spec.rs
+++ src/openapi/spec.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+
 use actix_web::HttpRequest;
-use oas3::spec::{MediaTypeExamples, ObjectOrReference, Operation, PathItem, Response};
+use oas3::spec::{Example, MediaTypeExamples, ObjectOrReference, Operation, PathItem, Response};
 
 pub type SpecResult<T> = Result<T, Box<dyn std::error::Error>>;
 
@@ -17,15 +19,15 @@ impl Spec {
         let path = req.uri().path();
         let method = req.method().as_str().to_lowercase();
         let media_type = "application/json";
-        let example_name = "default";
 
         Some(&self.spec)
             .and_then(load_path(path))
             .and_then(load_method(&method))
             .and_then(load_responses())
             .and_then(load_examples(&self.spec, media_type))
-            .and_then(|examples| examples.into_iter().next())
-            .and_then(get_example(example_name, &self.spec))
+            .and_then(find_example_match(req))
+            .and_then(|example| example.resolve(&self.spec).ok())
+            .and_then(|example| example.value)
     }
 }
 
@@ -37,7 +39,29 @@ fn load_spec(path: &str) -> Option<oas3::OpenApiV3Spec> {
 }
 
 fn load_path<'a>(path: &'a str) -> impl Fn(&oas3::OpenApiV3Spec) -> Option<PathItem> + 'a {
-    move |spec: &oas3::OpenApiV3Spec| spec.paths.clone().get(path).cloned()
+    move |spec: &oas3::OpenApiV3Spec| {
+        spec.paths
+            .iter()
+            .find(|(key, _)| match_url(path, &[*key]))
+            .map(|(_, value)| value.clone())
+    }
+}
+
+fn match_url(url: &str, routes: &[&str]) -> bool {
+    let url_parts: Vec<&str> = url.split('/').filter(|s| !s.is_empty()).collect();
+
+    for route in routes {
+        let route_parts: Vec<&str> = route.split('/').filter(|s| !s.is_empty()).collect();
+        if url_parts.len() == route_parts.len()
+            && route_parts
+                .iter()
+                .zip(url_parts.iter())
+                .all(|(r, u)| r.starts_with('{') && r.ends_with('}') || r == u)
+        {
+            return true;
+        }
+    }
+    false
 }
 
 fn load_method<'a>(method: &'a str) -> impl Fn(PathItem) -> Option<Operation> + 'a {
@@ -97,6 +121,94 @@ fn extract_response(
     }
 }
 
+/// Find the example that matches the request.
+///
+/// It matches the examples by comparing the request path, query,
+/// and headers with the example name.
+/// If the example name matches the request path, it returns the example.
+/// If the example name does not match the request path, it returns None.
+///
+/// # Matching exact route
+/// If the example name is the same as the request path, it returns the example.
+/// Example:
+/// - Example name: `/pets`
+/// - Request path: `/pets`
+/// - Returns the example
+///
+/// - Example name: `/pets`
+/// - Request path: `/pets/123`
+/// - Returns None
+fn find_example_match<'a>(
+    req: &'a HttpRequest,
+) -> impl Fn(Vec<MediaTypeExamples>) -> Option<ObjectOrReference<Example>> {
+    let path = req.uri().path().to_string();
+
+    let query = QueryString::from_request(req);
+
+    move |examples: Vec<MediaTypeExamples>| {
+        let mut default: Option<ObjectOrReference<Example>> = None;
+        for example in examples {
+            match example {
+                MediaTypeExamples::Examples { examples } => {
+                    for (example_name, e) in examples.iter() {
+                        // Match exact path
+                        if example_name == &path {
+                            return Some(e.clone());
+                        }
+
+                        // Match query parameters
+                        if query.match_example(&example_name) {
+                            return Some(e.clone());
+                        }
+
+                        // Match default example
+                        if example_name == "default" {
+                            default = Some(e.clone());
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+        default
+    }
+}
+
+struct QueryString {
+    params: HashMap<String, String>,
+}
+
+impl QueryString {
+    fn from_request(req: &HttpRequest) -> Self {
+        let mut params = HashMap::new();
+        for (key, value) in req.query_string().split('&').map(|pair| {
+            let mut split = pair.split('=');
+            (split.next().unwrap(), split.next().unwrap_or(""))
+        }) {
+            params.insert(key.to_string(), value.to_string());
+        }
+        Self { params }
+    }
+
+    fn match_example(&self, example_name: &str) -> bool {
+        if example_name.starts_with("query:") {
+            let query = example_name.trim_start_matches("query:");
+            let mut query_params = HashMap::new();
+            for pair in query.split('&').map(|pair| {
+                let mut split = pair.split('=');
+                (split.next().unwrap(), split.next().unwrap_or(""))
+            }) {
+                query_params.insert(pair.0.to_string(), pair.1.to_string());
+            }
+            self.params
+                .iter()
+                .all(|(key, value)| query_params.get(key).map_or(false, |v| v == value))
+        } else {
+            false
+        }
+    }
+}
+
 fn get_example<'a>(
     example_name: &'a str,
     spec: &'a oas3::OpenApiV3Spec,
@@ -116,9 +228,8 @@ fn get_example<'a>(
 
 #[cfg(test)]
 mod tests {
-    use actix_web::test::TestRequest;
-
     use super::*;
+    use actix_web::test::TestRequest;
 
     #[test]
     fn test_load_spec() {
@@ -140,6 +251,22 @@ mod tests {
             .as_ref()
             .and_then(load_path("/notfound"));
         assert!(path.is_none());
+    }
+
+    #[test]
+    fn test_load_path_with_params() {
+        let path = load_spec("tests/testdata/petstore.yaml")
+            .as_ref()
+            .and_then(load_path("/pets/{petId}"));
+        assert!(path.is_some());
+    }
+
+    #[test]
+    fn test_load_path_with_dynamic_params() {
+        let path = load_spec("tests/testdata/petstore.yaml")
+            .as_ref()
+            .and_then(load_path("/pets/123"));
+        assert!(path.is_some());
     }
 
     #[test]
@@ -181,13 +308,50 @@ mod tests {
     }
 
     #[test]
-    fn test_load_responses() {
-        let responses = load_spec("tests/testdata/petstore.yaml")
-            .as_ref()
-            .and_then(load_path("/pets"))
-            .and_then(load_method("get"))
-            .and_then(load_responses());
-        assert!(responses.is_some());
-        assert_eq!(responses.unwrap().len(), 2);
+    fn test_spec_with_path_params() {
+        let spec = Spec::from_path("tests/testdata/petstore.yaml").unwrap();
+        let req = TestRequest::with_uri("/pets/123").to_http_request();
+        let example = spec.get_example(&req);
+        assert!(example.is_some());
+    }
+
+    #[test]
+    fn test_spec_with_params_custom_example() {
+        let spec = Spec::from_path("tests/testdata/petstore.yaml").unwrap();
+        let req = TestRequest::with_uri("/pets/2").to_http_request();
+        let example = spec.get_example(&req).unwrap();
+
+        assert_eq!(
+            example["id"],
+            serde_json::Value::Number(serde_json::Number::from(2))
+        );
+    }
+
+    #[test]
+    fn test_spec_match_query_params() {
+        let spec = Spec::from_path("tests/testdata/petstore.yaml").unwrap();
+        let req = TestRequest::with_uri("/pets?page=1").to_http_request();
+        let res = spec.get_example(&req).unwrap();
+
+        let example = res.as_array().unwrap().get(0).unwrap();
+        assert_eq!(
+            example["id"],
+            serde_json::Value::Number(serde_json::Number::from(1))
+        );
+    }
+
+    #[test]
+    fn test_spec_match_query_params_with_multiple_params() {
+        let spec = Spec::from_path("tests/testdata/petstore.yaml").unwrap();
+        let req = TestRequest::with_uri("/pets?page=1&limit=1").to_http_request();
+        let res = spec.get_example(&req).unwrap();
+
+        let examples = res.as_array().unwrap();
+        assert_eq!(examples.len(), 1,);
+        let example = examples.get(0).unwrap();
+        assert_eq!(
+            example["id"],
+            serde_json::Value::Number(serde_json::Number::from(1))
+        );
     }
 }
